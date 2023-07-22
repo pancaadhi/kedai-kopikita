efec691c6e88e5d2a45f5a1df1f28998b7cebeb6
Jadex <jadex@pop-os.localdomain>
2023-07-22 21:26:41 +0700

    menambahkan fitur searching, products section, shopping cart dan item detail modal

--- js/script.js
+++ js/script.js
@@ -1,16 +1,73 @@
-// toggle class active
+// toggle class active on humberger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // when humberger on click
-document.querySelector("#humberger-menu").onclick = () => {
+document.querySelector("#humberger-menu").onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
-// klik di luar sidebar untuk menghilangkan nav
+// toggle class active on search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+// when search button on click
+document.querySelector("#search-button").onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+
+}
+
+// toggle class active on shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+// when shopping caret button on click
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+//click outside contains to remove navbar nav or search form
 const humberger = document.querySelector('#humberger-menu');
+const searchButton = document.querySelector('#search-button');
+const shoppingCartButton = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function (e) {
     if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+
+    if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+
+    if (!shoppingCartButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
 });
+
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+});
+
+// close button modal on click
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+// click outside modal content to close modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+}
